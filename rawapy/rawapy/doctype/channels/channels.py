b39c0145de4eca37ee848967eaf571dfdcd88455
rashedpro <rashedpro2025@gmail.com>
2023-12-17 16:16:00 +0300

    make filed unready only

--- rawapy/rawapy/doctype/channels/channels.py
+++ rawapy/rawapy/doctype/channels/channels.py
@@ -5,8 +5,9 @@
 from frappe.model.document import Document
 
 class Channels(Document):
-	def before_save(self):
-		codes=self.channels.split()
-		first_ch=[ch[0] for ch in codes]
-		self.code=''.join(first_ch).upper()
+    pass
+	# def before_save(self):
+	# 	codes=self.channels.split()
+	# 	first_ch=[ch[0] for ch in codes]
+	# 	self.code=''.join(first_ch).upper()
 		
